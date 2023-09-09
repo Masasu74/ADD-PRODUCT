2645a7db17c3d8ff1fdaed5be991bc50697dc8b7
Salomon Masasu <masasusalomon@gmail.com>
2023-09-10 00:44:01 +0200

    Added Php and Db with Docker

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
+<?php
+$conn = mysqli_connect('db', 'Salomon', '12345678', 'product');
+if(isset($_POST['submit'])){
+$productname=$_POST['product'];
+$sql="INSERT INTO Product (Product_Name) VALUES ('$productname')";
+$result=mysqli_query($conn,$sql);
+if($result){
+    echo '<script>alert("Product Added")</script>';
+
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <h1 class="title">ADD <br> <span>PRODUCT</span></h1>
-<input type="text" class="text" placeholder="Enter The Name of The Product" required > <br>
-<button name="submit" class="btn">ADD</button>
+        <form action="" method="post">
+            <input type="text" class="text" name="product" placeholder="Enter The Name of The Product" required> <br>
+            <button name="submit" class="btn">ADD</button>
+        </form>
+
     </div>
 </body>
+
 </html>
