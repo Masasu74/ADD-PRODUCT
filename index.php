d8f8a7d4b4ea9fb153e3e82a958634cc584716d1
Salomon Masasu <masasusalomon@gmail.com>
2023-09-10 00:06:30 +0200

    HTML $ CSS

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">ADD <br> <span>PRODUCT</span></h1>
+<input type="text" class="text" placeholder="Enter The Name of The Product" required > <br>
+<button name="submit" class="btn">ADD</button>
+    </div>
+</body>
+</html>
